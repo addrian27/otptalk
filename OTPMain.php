56aaf9e0a5da45a6667041862998ad90544edf1a
adrian <adrian@adrians-MacBook-Air.local>
2016-06-29 15:23:16 +0300

    second commit

---
+++ OTPMain.php
@@ -0,0 +1,23 @@
+<?php
+class OTPMain {
+
+   __construct() { }
+
+  public static function encryptWithOTP($otp, $message) {
+   $scrambledMessage = '';
+   //Do stuff
+
+    return $scrambledMessage;
+  }
+
+  public static function decryptWithOTP($otp, $message) {
+  $unscrambledMessage = '';
+  //Do stuff
+
+   return $unscrambledMessage;
+  }
+
+  //...
+
+}
+?>
